1f5fe05ce9036bf68fb82622bdc5268512d80762
Lincoln Santander <liedsaal92@hotmail.es>
2022-03-13 17:28:36 -0500

    primer commit de java

---
+++ src/test/java/com/fundamentos/platzi/nttdata/NttdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.platzi.nttdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
